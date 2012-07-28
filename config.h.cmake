cebd59d825254ad22b1107104b26ce87046a8dd6
Masahiro Ide <imasahiro9@gmail.com>
2012-07-28 16:07:50 +0900

    added CMakeLists.txt

---
+++ config.h.cmake
@@ -0,0 +1,11 @@
+/* build revision */
+#cmakedefine GPERL_REVISION  ${GPERL_REVISION}
+
+/* The size of `int', as computed by sizeof. */
+#cmakedefine SIZEOF_INT ${SIZEOF_INT}
+
+/* The size of `long', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG ${SIZEOF_LONG}
+
+/* The size of `void*', as computed by sizeof. */
+#cmakedefine SIZEOF_VOIDP ${SIZEOF_VOIDP}
